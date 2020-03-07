05ff1d7e8b87f1778fbb26de87b9ebe2277f07e3
Joel Hobgood <jhobgoo@gmail.com>
2020-03-07 21:36:37 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,12 @@
 class Dog
 
   @@all = []
-  @@print_all = []
 
   attr_accessor :dog_name
 
   def initialize(dog_name)
     @name = dog_name
     @@all << self
-    @@print_all << self
   end
   
   def self.all
@@ -20,8 +18,7 @@ class Dog
   end
   
   def self.print_all
-    @@print_all << self
-    print self.name
+    print @@all
   end
   
 end
